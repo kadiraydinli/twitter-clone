75ea3ed6cf7150aa716dfec23db1121d3f85a4f8
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-04-15 18:05:47 +0300

    usePosts bug fixed

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -3,7 +3,7 @@ import useSwr from "swr";
 import fetcher from "@/libs/fetcher";
 
 const usePosts = (userId?: string) => {
-    const url = userId ? `/api/posts?userId:${userId}` : '/api/posts';
+    const url = userId ? `/api/posts?userId=${userId}` : '/api/posts';
     const { data, error, isLoading, mutate } = useSwr(url, fetcher);
 
     return {
