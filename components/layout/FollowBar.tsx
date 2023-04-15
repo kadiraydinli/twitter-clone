fab6dae9edc45c9790a875495e9044af0dbc051e
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-04-15 03:18:16 +0300

    FollowBar users list map

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -1,11 +1,46 @@
+import { useRouter } from "next/router";
+
+import Avatar from "../Avatar";
+
+import useUsers from "@/hooks/useUsers";
+import { useCallback } from "react";
+
 interface FollowBarProps {}
 
 const FollowBar: React.FC<FollowBarProps> = () => {
+  const router = useRouter();
+  const { data: users = [] } = useUsers();
+
+  const onClick = useCallback(
+    (userId: string) => {
+      const url = `/users/${userId}`;
+
+      router.push(url);
+    },
+    [router]
+  );
+
+  if (!users.length) return null;
+
   return (
     <div className="px-6 py-4 hidden lg:block">
       <div className="bg-neutral-800 rounded-xl p-4">
         <h2 className="text-white text-xl font-semibold">Who to follow</h2>
-        <div className="flex flex-col gap-6 mt-4 ">{/* TODO USER LIST */}</div>
+        <div className="flex flex-col gap-6 mt-4 ">
+          {users.map((user: Record<string, any>) => (
+            <div
+              key={user.id}
+              onClick={() => onClick(user.id)}
+              className="flex flex-row gap-4 cursor-pointer"
+            >
+              <Avatar userId={user.id} />
+              <div className="flex flex-col">
+                <p className="text-white font-semibold text-sm">{user.name}</p>
+                <p className="text-neutral-400 text-sm">@{user.username}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
