936c5eee4210b13914d6bfe9680ff0bf39204b1a
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-04-15 16:59:54 +0300

    SidebarTweetButton bg color fix

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -19,7 +19,7 @@ const SidebarTweetButton: React.FC<SidebarTweetButtonProps> = () => {
       <div className="rounded-full lg:hidden mt-6 w-14 h-14 p-4 flex items-center justify-center bg-sky-300 hover:bg-opacity-80 transition cursor-pointer">
         <FaFeather size={24} color="white" />
       </div>
-      <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-300 hover:bg-opacity-80 transition cursor-pointer">
+      <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
         <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
           Tweet
         </p>
