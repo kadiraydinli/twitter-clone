877a5110ed09818474626ee1340494cb73c5a115
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-04-15 20:29:43 +0300

    post detail

---
+++ pages/posts/[postId].tsx
@@ -0,0 +1,46 @@
+import { useMemo } from "react";
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+import usePost from "@/hooks/usePost";
+import Header from "@/components/Header";
+import PostItem from "@/components/posts/PostItem";
+import Form from "@/components/Form";
+
+const PostView: React.FC = () => {
+  const router = useRouter();
+  const { postId } = router.query;
+
+  const { data: fetchedPost, isLoading } = usePost(postId as string);
+
+  const loading = isLoading || !fetchedPost;
+
+  const loaderContent = useMemo(
+    () => (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="ligthBlue" size={80} />
+      </div>
+    ),
+    []
+  );
+
+  return (
+    <>
+      <Header showBackArrow label="Tweet" />
+      {loading ? (
+        loaderContent
+      ) : (
+        <>
+          <PostItem data={fetchedPost} />
+          <Form
+            postId={postId as string}
+            isComment
+            placeholder="Tweet your reply"
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default PostView;
