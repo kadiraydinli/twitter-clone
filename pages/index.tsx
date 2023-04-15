f281595c9acc7956c6a1ce5545423a2d8344c0ce
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-04-15 17:16:22 +0300

    Form component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Form from "@/components/Form";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="">
       <Header label="Home" />
+      <Form placeholder="What's happening?" />
     </div>
   );
 }
